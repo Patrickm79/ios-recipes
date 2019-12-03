7ff64a4932277a353dd362c80470ce3ecfb2ac67
Patrick Millet <lohs79seahawk@gmail.com>
2019-12-02 18:28:15 -0600

    got UI built, classes created. Working on MVP

---
+++ Recipes/Recipes/Controllers/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Patrick Millet on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    // MARK: - Actions
+    
+    @IBAction func didEndAction(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Recipes/Recipes/Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Patrick Millet on 12/2/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    
+    @IBOutlet weak var textViewOutlet: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
